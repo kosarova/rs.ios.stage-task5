b1d1d30a6f8b589276abbda36310c87acc969d56
Kobzarev Roman <krv2306@gmail.com>
2021-06-14 21:59:54 +0300

    add 1-3 task

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,18 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var result = 0
+            for i in 0..<prices.count{
+                var max = 0
+                for j in i + 1..<prices.count{
+                    if (prices[j] - prices[i] > max){
+                        max = prices[j] - prices[i]
+                    }
+                }
+                result += max
+            }
+                
+                return result
+            
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-3/Knapsack.swift
@@ -17,6 +17,63 @@ public final class Knapsack {
     }
     
     func findMaxKilometres() -> Int {
-        return -1
+        var foodsInBackpack: [[Int]] = Array(repeating: Array(repeating: 0, count: maxWeight + 1),
+            count: foods.count + 1)
+        var drinksInBackpack: [[Int]] = Array(repeating: Array(repeating: 0, count: maxWeight + 1),
+            count: drinks.count + 1)
+        
+        for i in 0..<foods.count{
+            let currentFood = foods[i]
+            for j in 1...maxWeight{
+                if (((j - currentFood.weight > 0 && foodsInBackpack[i][j - currentFood.weight] > 0) ||
+                j - currentFood.weight == 0) && foodsInBackpack[i][j - currentFood.weight] + currentFood.value > foodsInBackpack[i][j]){
+                    foodsInBackpack[i + 1][j] = foodsInBackpack[i][j - currentFood.weight] + currentFood.value
+                }
+                else {
+                    foodsInBackpack[i + 1][j] = foodsInBackpack[i][j]
+                }
+            }
+        }
+        
+        for i in 0..<drinks.count{
+            let currentDrink = drinks[i]
+            for j in 1...maxWeight{
+                if (((j - currentDrink.weight > 0 && drinksInBackpack[i][j - currentDrink.weight] > 0) ||
+                j - currentDrink.weight == 0) && drinksInBackpack[i][j - currentDrink.weight] + currentDrink.value > drinksInBackpack[i][j]){
+                    drinksInBackpack[i + 1][j] = drinksInBackpack[i][j - currentDrink.weight] + currentDrink.value
+                }
+                else {
+                    drinksInBackpack[i + 1][j] = drinksInBackpack[i][j]
+                
+                }
+            }
+        }
+        
+        for i in 1...maxWeight{
+            if (foodsInBackpack[foods.count][i] < foodsInBackpack[foods.count][i - 1]){
+                foodsInBackpack[foods.count][i] = foodsInBackpack[foods.count][i - 1]
+            }
+        }
+        
+        for i in 1...maxWeight{
+            if (drinksInBackpack[drinks.count][i] < drinksInBackpack[drinks.count][i - 1]){
+                drinksInBackpack[drinks.count][i] = drinksInBackpack[drinks.count][i - 1]
+            }
+        }
+        
+        var max = 0
+        for i in 0...maxWeight{
+            let currentFood = foodsInBackpack[foods.count][i]
+            let currentDrink = drinksInBackpack[drinks.count][maxWeight - i]
+            var result = currentFood
+            if (currentDrink < currentFood){ result = currentDrink }
+            if (result > max) {
+                max = result
+                
+            }
+        }
+        
+        return max
+        
     }
 }
